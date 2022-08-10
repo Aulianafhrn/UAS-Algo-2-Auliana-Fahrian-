6a51a310f95228b14d01ed160f419d2d8f09deb9
Aulianafhrn <94036380+Aulianafhrn@users.noreply.github.com>
2022-08-10 10:45:14 +0700

    Update koneksi.java

--- UAS Algo Auliana Fahrian/uas_algo_aulianafahrian/koneksi.java
+++ UAS Algo Auliana Fahrian/uas_algo_aulianafahrian/koneksi.java
@@ -27,3 +27,37 @@ public class koneksi
 }
 
 
+
+public class koneksi  
+{
+    public  static String a = "";
+    public koneksi()
+    {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql:mysql://localhost/test";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName(driver);
+            String query = "SELECT * FROM `gerak` WHERE `id` = 1";
+            try{
+                Connection conn = DriverManager.getConnection(url, user, password);
+                Statement stmt = conn.createStatement();
+                ResultSet kunci = stmt.executeQuery(query); 
+                
+                while(gerak.next()){                   
+                    a = gerak.getString("gerak");
+                }  
+                
+            } catch (SQLException e) {
+                a="gagal";
+                System.out.println(e);
+            }
+            
+
+        } catch (ClassNotFoundException e) {
+            a="gagal";
+            System.out.println(e);
+        }//end try catch
+    }
+
